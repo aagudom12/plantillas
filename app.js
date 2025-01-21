f31a7b52e25007e3a91345cfd8311ed425e80532
Alfredo <agudo.mota1314@gmail.com>
2025-01-21 16:26:43 +0000

    V1

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+app.set('view engine','ejs')
+app.get('/', function (req, res) {
+    const datos = [
+        {nombre: 'Ana', apellidos:'Garcia'},
+        {nombre: 'Juan', apellidos:'López'}
+    ]
+  res.render('index',{datos:datos});
+})
+
+app.get('/saludo', function (req, res) {
+    res.render('saludo',{hora:16});
+})
+
+app.get('/productos', function (req, res) {
+    const producto = {id: '1', nombre:'coche', colores:['rojo','negro']}
+    res.render('productos',{producto:producto});
+})
+
+app.listen(3000)
